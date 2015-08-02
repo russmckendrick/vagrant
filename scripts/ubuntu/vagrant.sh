61c2f11ab0add181cd809675667ed68ed281bbb7
Russ McKendrick <russ@mckendrick.io>
2015-08-02 18:41:22 +0100

    Fixing keys on vagrant user

--- scripts/ubuntu/vagrant.sh
+++ scripts/ubuntu/vagrant.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
 date > /etc/vagrant_box_build_time
-useradd -G sudo -p $(perl -e'print crypt("vagrant", "vagrant")') -m -s /bin/bash -N vagrant
-mkdir -pm 700 /home/vagrant/.ssh
-curl -Lo /home/vagrant/.ssh/authorized_keys \
-  'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
-chmod 0600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant:vagrant /home/vagrant/.ssh
+cd ~vagrant
+mkdir -m 700 .ssh
+curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o .ssh/authorized_keys
+chmod 600 .ssh/authorized_keys
+chown -R vagrant:vagrant .
 apt-get -y install nfs-common
