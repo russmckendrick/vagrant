19519df2fbff447eb2b5f1d56ee767f6be6618b6
Russ McKendrick <russ@mckendrick.io>
2015-08-06 06:27:17 +0100

    Adding CentOS 7 with no updates

---
+++ scripts/centos7/base.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eux
+
+# Install some basics and update
+
+yum install -y wget nfs-utils rpcbind git vim-enhanced vim-common net-tools
+systemctl enable rpcbind
+systemctl enable nfs-lock
+systemctl enable nfs-idmap
+
+# add 'single-request-reopen' so it is included when /etc/resolv.conf is generated
+echo 'RES_OPTIONS="single-request-reopen"' >> /etc/sysconfig/network
+service network restart
+echo 'Slow DNS fix applied (single-request-reopen)'
+
+# update root certs
+wget -O/etc/pki/tls/certs/ca-bundle.crt http://curl.haxx.se/ca/cacert.pem
