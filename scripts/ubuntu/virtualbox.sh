e706633f09bc55cb4c32ddd837894ce42f2755fa
Russ McKendrick <russ@mckendrick.io>
2015-08-09 10:09:30 +0100

    updating vbox additions.

--- scripts/ubuntu/virtualbox.sh
+++ scripts/ubuntu/virtualbox.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 apt-get -y install --no-install-recommends libdbus-1-3
 aptitude -y install dkms
-apt-get -y install virtualbox-guest-utils
+/etc/init.d/virtualbox-ose-guest-utils stop
+rmmod vboxguest
+aptitude -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
+aptitude -y install dkms
+mount -o loop $VBOX_ISO /mnt
+yes|sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
 sudo rm -rf VBoxGuestAdditions.iso
