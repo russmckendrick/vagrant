ec299d2e5a5268339e408e304e32cfbf6cebf90c
Russ McKendrick <russ@mckendrick.io>
2015-08-09 11:35:18 +0100

    Revert "updating vbox additions."
    This reverts commit e706633f09bc55cb4c32ddd837894ce42f2755fa.

--- scripts/ubuntu/virtualbox.sh
+++ scripts/ubuntu/virtualbox.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 apt-get -y install --no-install-recommends libdbus-1-3
 aptitude -y install dkms
-/etc/init.d/virtualbox-ose-guest-utils stop
-rmmod vboxguest
-aptitude -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
-aptitude -y install dkms
-mount -o loop $VBOX_ISO /mnt
-yes|sh /mnt/VBoxLinuxAdditions.run
-umount /mnt
+apt-get -y install virtualbox-guest-utils
 sudo rm -rf VBoxGuestAdditions.iso
